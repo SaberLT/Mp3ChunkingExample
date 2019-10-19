0dd9a2fde228e5cb7ea8044a2dcd0834a31b9029
SaberMK <pnegebodyguard@gmail.com>
2019-10-20 02:30:13 +0300

    Initial commit: Full File Loading W0rking

---
+++ Streaming/ClientApp/src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  
+  public file : ArrayBuffer;
+  public text : string;
+
+  public test = () => {
+    let params = {
+      data: "parameter"
+    };
+
+    this.http.get(this.baseUrl+'api/audio/test', {
+       responseType: 'text',
+       params
+    }).subscribe((result : string) => {
+      this.text = result;
+      console.log(result);
+    }, error => console.error(error));
+  }
+
+  public audioContext: any;
+  public source : any;
+
+  public loadStream = () => {
+    let params = {
+      file: "djip.mp3"
+    }
+
+    this.http.get(this.baseUrl+"api/audio/loadFile", {
+      responseType: 'arraybuffer',
+      params
+    }).subscribe((result : ArrayBuffer) => {
+      this.file = result;
+      console.log(result);
+      var promise = this.audioContext.decodeAudioData(result, (buff) => {
+        this.source.buffer = buff;
+        this.source.connect(this.audioContext.destination);
+        this.source.start();
+      }, error => console.log(error))
+
+      // this.file = result;
+      // console.log(result);
+
+      // var promise = this.audioContext.decodeAudioData(this.file, (buff)=>{
+      //   this.source.buffer = buff;
+      //   this.source.connect(this.audioContext.destination);
+      //   this.source.loop = true;
+      // }, (error) => {
+      //   console.error(error);
+      // });
+      // if(promise)
+      //   promise.catch((error) => {
+      //     console.log(error);
+      //   })
+
+    }, error => console.error(error));
+  }
+
+  gesture = () => {
+    // this.audioContext = new AudioContext();
+    // this.source = this.audioContext.createBufferSource();
+
+    this.audioContext = new (window['AudioContext'] || window['webkitAudioContext']);
+    this.source = this.audioContext.createBufferSource();
+
+    console.log('Gesture complete!');
+  }
+
+  ngOnInit() {
+    
+  }
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string
+    ) {
+      
+
+    //http.get<string>()
+  }
+}
---
+++ Streaming/Controllers/AudioController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Streaming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AudioController : ControllerBase
+    {
+        [HttpGet("test")]
+        public string Test([FromQuery] string data)
+        {
+            return "test";
+            //return new Resp()
+            //{
+            //    Result = "test"
+            //};
+        }
+
+        //[HttpGet("loadFile")]
+        //public byte[] LoadFile([FromQuery] string file)
+        //{
+        //    var bytes = System.IO.File.ReadAllBytes($"Audio\\{file}");
+        //    return bytes;
+        //}
+
+
+        [HttpGet("loadFile")]
+        public FileResult LoadFile([FromQuery] string file)
+        {
+            var bytes = System.IO.File.ReadAllBytes($"Audio\\{file}");
+            return File(bytes, "audio/mpeg");
+        }
+    }
+
+}
